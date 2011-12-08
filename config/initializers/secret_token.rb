4a1ab02018fda1b982d615420ee9fd6b0febf703
Quy Phan <c_qphan@qualcomm.com>
2011-12-07 21:08:03 -0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quyphan::Application.config.secret_token = '1c0c12f186d560a9e243c8b2f6581d7c0c6565ed26c56d7d59639777f8ec7f91f25ee85731fb37a8dcc7615d5421d8549b6a3234951d228d31763d2f73468c1d'
