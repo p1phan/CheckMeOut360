d715678ceb19e002de3f4754291f6522bdb7f176
Quy Phan <c_qphan@qualcomm.com>
2011-12-13 00:52:29 -0800

    Places changed, added tooltip and also the animation on markers

--- app/assets/javascripts/places.js
+++ app/assets/javascripts/places.js
@@ -1,12 +1,20 @@
 var map;
-var places_lat_long_array;
-var marker;
+var places_holder;
+var marker = null;
+var iterator;
+var infowindow = null;
 
 $(document).ready(function() {
+  places_holder = $('#dummy_places_holder .place_holder');
   google.maps.event.addDomListener(window, 'load',  function() {
     init_google_map();
-    place_set_of_markers();
+    /* now inside your initialise function */
+    infowindow = new google.maps.InfoWindow({
+    content: "holding..."
+    });
+    drop();
   });
+  
 });
 
 function init_google_map() {
@@ -28,13 +36,35 @@ function populate_lat_long(lat, lon) {
   $("#place_longitude").val(lon);
 }
 
-function place_set_of_markers() {
-  var places_holder = $('#dummy_places_holder .place_holder');
-  places_holder.each(function(place) {
-    marker = new google.maps.Marker({
-      position: new google.maps.LatLng($(this).attr("lat"), $(this).attr("long")),
-      map: map,
-      draggable:false
-    });
+function place_marker() {
+  marker = new google.maps.Marker({
+    position: new google.maps.LatLng($(places_holder[iterator]).attr("lat"), $(places_holder[iterator]).attr("long")),
+    map: map,
+    title: $(places_holder[iterator]).text(),
+    animation: google.maps.Animation.DROP,
+    draggable:false
   });
+  iterator++;
+  return marker;
+}
+
+function toggleBounce() {
+  if (marker.getAnimation() != null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+}
+
+function drop() {
+  iterator = 0;
+  marker = null;
+  for (var i = 0; i < places_holder.length; i++) {
+    var marker = place_marker();
+    // console.log(marker);
+    google.maps.event.addListener(marker, 'click', function (a,b) {
+      infowindow.setContent("<h3>" + this.title + "</h3>");
+      infowindow.open(map, this);
+    });
+  }
 }
--- app/models/place.rb
+++ app/models/place.rb
@@ -4,5 +4,12 @@ class Place < ActiveRecord::Base
   geocoded_by :address, :latitude => :lat, :longitude => :long
   after_validation :geocode
   validates :address, :presence => :true, :length => {:minimum => 4, :message => "Name of Place is too short!"}, :uniqueness => {:message => "Place was already created!"}
-
+  before_save :set_name_if_none_given
+  
+  def set_name_if_none_given
+    unless name
+      address_split = address.split(" ")
+      self.name = address_split.try(:first).to_s + address_split.try(:second).to_s
+    end
+  end
 end
