f8f63966307f73d09c98f9c81c26b640f9c583c8
Quy Phan <quyminhphan@gmail.com>
2012-04-22 12:44:46 -0700

    refactored privacy settings, and accordion

--- app/models/checkin.rb
+++ app/models/checkin.rb
@@ -6,41 +6,35 @@ class Checkin < ActiveRecord::Base
 
   def self.build_checkins(checkins)
     ActiveRecord::Base.transaction do
-    @all_checkins = Checkin.all
-    @all_places = Place.all
-    @all_users = User.all
+      @all_checkins = Checkin.all
+      @all_places = Place.all
+      @all_users = User.all
     
-    checkins.each do |checkin|
-      facebook_checkin = find_or_init_checkin(checkin.id)
-      if facebook_checkin.new_record?
-        facebook_place = find_or_init_place(checkin.place.id)
-        facebook_checkin.place = facebook_place
-        if facebook_place.new_record?
-          facebook_place.build_from_checkin(checkin)
-          facebook_place.save!
-          @all_places << facebook_place
-        end
-        facebook_user_tags = checkin.tags << checkin.from
-        facebook_user_tags.each do |user|
-          facebook_user = find_or_init_user(user.id)
-          if facebook_user.new_record?
-            facebook_user.uid = user.id
-            facebook_user.name = user.name
-            facebook_user.email = user.name.downcase.gsub(" ", "#{rand(10).to_s}")+"#{rand(10).to_s}" + "@checkmeout360.com"
-            facebook_user.password = user.name.downcase.gsub(" ", "")
-            facebook_user.password_confirmation = user.name.downcase.gsub(" ", "")
-            # facebook_user.remote_profile_picture_url = graph.get_picture(user.id, :type => "large")
-            facebook_user.save!
-            @all_users << facebook_user
+      checkins.each do |checkin|
+        facebook_checkin = find_or_init_checkin(checkin.id)
+        if facebook_checkin.new_record?
+          facebook_place = find_or_init_place(checkin.place.id)
+          facebook_checkin.place = facebook_place
+          if facebook_place.new_record?
+            facebook_place.build_from_checkin(checkin)
+            facebook_place.save!
+            @all_places << facebook_place
           end
-          facebook_checkin.users << facebook_user
+          facebook_user_tags = checkin.tags << checkin.from
+          facebook_user_tags.each do |user|
+            facebook_user = find_or_init_user(user.id)
+            if facebook_user.new_record?
+              facebook_user = populate_user(user)
+              @all_users << facebook_user
+            end
+            facebook_checkin.users << facebook_user
+          end
+          facebook_checkin.message = checkin.message
+          facebook_checkin.created_at = checkin.created_time
+          facebook_checkin.save!
+          @all_checkins << facebook_checkin
         end
-        facebook_checkin.message = checkin.message
-        facebook_checkin.created_at = checkin.created_time
-        facebook_checkin.save!
-        @all_checkins << facebook_checkin
       end
-    end
     end
   end
   
@@ -61,29 +55,11 @@ class Checkin < ActiveRecord::Base
       end
       facebook_checkins_for_user = facebook_checkins_for_user.next_page
     end
-    
-    # next_facebook_checkins_for_user = nil
-    # index = 0
-    # while (true)
-    #   next_facebook_checkins_for_user = facebook_checkins_for_user.next_page
-    #   return @checkins if next_facebook_checkins_for_user.size == 0
-    #   if index == 0
-    #     fcl = user.facebook_checkin_logs.build(next: facebook_checkins_for_user.paging['next'], previous: nil, done: false)
-    #     fcl.current = next_facebook_checkins_for_user.paging['previous'] if next_facebook_checkins_for_user.size != 0
-    #   else
-    #     fcl = user.facebook_checkin_logs.build(current: facebook_checkins_for_user.paging['next'],
-    #                next: next_facebook_checkins_for_user.paging['next'], previous: next_facebook_checkins_for_user.paging['previous'], done: false)
-    #   end
-    #   facebook_checkins_for_user = next_facebook_checkins_for_user
-    #   index = index + 1
-    #   fcl.save!
-    # end
+
     return @checkins
   end
 
   def self.store_checkins(token, facebook_checkin_url)
-    graph = Koala::Facebook::API.new(token)
-    
     raw_request = RestClient.get(facebook_checkin_url)
     checkins_json = ActiveSupport::JSON.decode(raw_request)
     @checkins = []
@@ -130,6 +106,16 @@ class Checkin < ActiveRecord::Base
     else
       return User.new
     end
+  end
+  
+  def populate_user(user)
+    facebook_user.uid = user.id
+    facebook_user.name = user.name
+    facebook_user.email = user.name.downcase.gsub(" ", "#{rand(10).to_s}")+"#{rand(10).to_s}" + "@checkmeout360.com"
+    facebook_user.password = user.name.downcase.gsub(" ", "")
+    facebook_user.password_confirmation = user.name.downcase.gsub(" ", "")
+    facebook_user.save!
+    return facebook_user
   end
   
   def self.oldest_checkin
