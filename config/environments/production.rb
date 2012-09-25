192527c2e7a784235df8b706569b766d64760c81
Quy Phan <quyminhphan@gmail.com>
2012-09-24 20:52:27 -0700

    bug fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,10 +31,10 @@ CheckMeOut360::Application.configure do
   # config.force_ssl = true
 
   # See everything in the log (default is :info)
-  # config.log_level = :debug
+  config.log_level = :debug
 
   # Use a different logger for distributed setups
-  # config.logger = SyslogLogger.new
+  config.logger = SyslogLogger.new
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
