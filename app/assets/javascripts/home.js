e4b02f1559a206b08702a247271c65a2f1b9a370
Phu Phan <Phu_Phan@playstation.sony.com>
2012-10-01 15:53:54 -0700

    More Layout Fix for the Who page.

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -4,22 +4,25 @@ CHECK_ME_OUT_360.home = {
     $('#main_footer').hide();
     $('#profile_banner').hide();
     $('#inner_body').hide();
-    
+
     $('.page-header').removeClass("partial_background");
-  
+
     $('.page-header').addClass("full_background");
   },
 
   who_action: function() {
     $("#myCarousel").carousel({
       slid: function() {
-        console.log('slid!!!!');
         $('#full_description_container').empty();
       }
     });
     $('a.display_description').click(function() {
       var id = $(this).data('id') + '_all_description';
       $('#full_description_container').empty().append($("#" + id).text())
+    });
+
+    $("#members_navbar").scrollspy({
+      offset: 10
     });
   }
 }
